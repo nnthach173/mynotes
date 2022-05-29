f5a6a7048bcd78d9ba35a1446cb8189f6c7f1cd5
nnthach173 <83439525+nnthach173@users.noreply.github.com>
2022-05-29 12:29:31 +0700

    verify bug

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,11 +36,14 @@ class HomePage extends StatelessWidget {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
             // final user = FirebaseAuth.instance.currentUser;
-
-            // if (user?.emailVerified ?? false) {
-            //   return const Text('Done');
+            // if (user != null) {
+            //   if (user.emailVerified) {
+            //     print('Email is verified');
+            //   } else {
+            //     return const VerifyEmailView();
+            //   }
             // } else {
-            //   return const VerifyEmailView();
+            //   return const LoginView();
             // }
             return const LoginView();
           default:
